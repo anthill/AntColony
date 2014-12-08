318ba2c64e366b216ad9c9b8f034548b0ad0c772
Romain Crestey <rcrestey@gmail.com>
2014-12-08 09:34:03 +0100

    added global reset function

--- index.js
+++ index.js
@@ -27,18 +27,13 @@ module.exports = function init(containerElement, options){
 	return {
 		togglePlayPause: function(){ render.togglePlayPause() },
 		changeOptions: function(opts){
-			if (opts.nbStart === undefined && opts.nbRand === undefined){
-				// modify population
-				render.modifyAnts(opts);
-			}
-			else{
-				// reset previous animation
-				render.reset();
+			// modify population
+			render.modifyAnts(opts);
+		},
+		reset: function(opts){
+			render.reset();
 
-				// reset elements
-				_init(containerElement, opts);
-				
-			}
+			_init(containerElement, opts);
 		}
 	};
 };
