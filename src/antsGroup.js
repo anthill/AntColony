6ac699a0b007e1c007d000ef4beb0c56c9c4af7f
Romain Crestey <rcrestey@gmail.com>
2015-01-18 17:16:39 +0100

    cleaning

--- src/antsGroup.js
+++ src/antsGroup.js
@@ -1,16 +1,8 @@
 'use strict'
 
-// var antFunction = require('./ant.js');
-
-// var NBANTS = 4000;
-
 module.exports = function (Ant) {
 
-	// var Ant = antFunction(container, pointsInfos, options);
 	var nbAntsPerStep = 100;
-
-	// var population = new Array(options.nbAnts);
-	// var possibleStartPointsId = pointsInfos.possibleStartPointsId;
 
 	function createGroup(population){
 		for (var i = 0; i < nbAntsPerStep; i++) {
